3c06d1878453fc111d41a8f2d167856aa258da90
Prince Ravi Leow <princeravileow@outlook.com>
2026-01-24 01:56:53 +0100

    gs2 script uses grounding-dino-base instead of tiny

--- script/gs2/chicken_tracking_demo_mps.py
+++ script/gs2/chicken_tracking_demo_mps.py
@@ -184,7 +184,8 @@ def main():
     image_predictor = SAM2ImagePredictor(sam2_image_model)
 
     # Load GroundingDINO on selected device
-    model_id = "IDEA-Research/grounding-dino-tiny"
+    # model_id = "IDEA-Research/grounding-dino-tiny"
+    model_id = "IDEA-Research/grounding-dino-base"
     processor = AutoProcessor.from_pretrained(model_id)
     grounding_model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(
         device
